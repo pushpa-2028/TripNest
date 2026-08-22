523c0e70a5c6d81b66566350fda28be5d954f6f5
pushpa-2028 <pckaradi@gmail.com>
2026-08-22 21:02:25 +0530

    Fix production API URL

--- tripnest-frontend/src/api.js
+++ tripnest-frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:8080/api",
+    "https://tripnest-fird.onrender.com/api",
   timeout: 15000
 });
 
